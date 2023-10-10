a0b051fe68414bbef953a433a6f40e5c1fbe52fa
hee-chan123 <sk9y9y@gmail.com>
2023-10-11 08:40:50 +0900

    게시글 작성, 수정, 답변 게시글 작성시 공백 체크

--- src/main/java/com/project/ownote/board/Controller/BoardController.java
+++ src/main/java/com/project/ownote/board/Controller/BoardController.java
@@ -1,115 +1,166 @@
 package com.project.ownote.board.Controller;
 
 import com.project.ownote.board.dto.Board;
-import com.project.ownote.board.dao.BoardDao;
 import com.project.ownote.board.page.BoardPage;
 import com.project.ownote.board.page.ListBoard;
+import com.project.ownote.board.service.BoardService;
+import com.project.ownote.emp.login.dto.AuthInfo;
+import com.project.ownote.emp.login.dto.Emp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class BoardController {
 
     @Autowired
-    BoardDao boardDao;
+    BoardService boardService;
 
     @Autowired
     ListBoard listBoard;
 
     @GetMapping("/board/boardmain") //게시판 메인
     public String boardMain(Model model){
-        List<Board> boardList = boardDao.selectAll();
+        List<Board> boardNotice = boardService.select(0, 5, "공지사항");
+        List<Board> boardForum = boardService.select(0, 5, "자유게시판");
+        List<Board> boardQa = boardService.select(0, 5, "Q&A");
 
-        model.addAttribute("boardList", boardList);
+        model.addAttribute("boardNotice", boardNotice);
+        model.addAttribute("boardForum", boardForum);
+        model.addAttribute("boardQa", boardQa);
+
         return "board/boardMain";
     }
 
     @GetMapping("/board/boardView/{boardNum}") //게시판 뷰
-    public String View(@PathVariable Long boardNum, Model model){
-        Board board = boardDao.selectByNum(boardNum);
-        boardDao.hitPlus(boardNum);
+    public String View(@PathVariable Long boardNum, Model model, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Emp emp = boardService.selectEmp(empId);
+        Board board = boardService.selectByNum(boardNum);
+        boardService.hitPlus(boardNum);
 
+        model.addAttribute("emp", emp);
         model.addAttribute("board", board);
         return "board/boardView";
     }
 
     @GetMapping("/board/boardwriteform") //게시판 글 쓰기 폼
-    public String noticeWriteForm(){
+    public String WriteForm(Model model, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Emp emp = boardService.selectEmp(empId);
+
+        model.addAttribute("emp", emp);
         return "board/boardWrite";
     }
 
     @PostMapping("/board/boardwrite") //게시판 글 쓰기
-    public String noticeWrite(@ModelAttribute("board") Board board){
-        boardDao.write(board);
-        boardDao.parentNumUpdate(boardDao.maxBoardNum());
-        return "redirect:/board/boardmain";
+    public String Write(@ModelAttribute("board") Board board, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+
+        boardService.write(board, empId);
+        boardService.parentNumUpdate(boardService.maxBoardNum());
+
+        switch (board.getBoardDivision()) {
+            case "공지사항":
+                return "redirect:/board/noticeList";
+            case "자유게시판":
+                return "redirect:/board/forumList";
+            case "Q&A":
+                return "redirect:/board/qaList";
+            default:
+                return "redirect:/board/boardmain";
+        }
     }
 
     @GetMapping("/board/boardupdate/{boardNum}") //게시판 업데이트 폼
-    public String noticeUpdateForm(@PathVariable Long boardNum, Model model){
-        Board board = boardDao.selectByNum(boardNum);
+    public String noticeUpdateForm(@PathVariable Long boardNum, Model model, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Emp emp = boardService.selectEmp(empId);
+        Board board = boardService.selectByNum(boardNum);
 
+        model.addAttribute("emp", emp);
         model.addAttribute("board", board);
         return "board/boardUpdate";
     }
 
     @PostMapping("/board/boardupdate/{boardNum}") //게시판 업데이트
     public String noticeUpdate(@ModelAttribute("board") Board board){
-        boardDao.update(board);
-        return "redirect:/board/boardmain";
+        boardService.update(board);
+
+        switch (board.getBoardDivision()) {
+            case "공지사항":
+                return "redirect:/board/noticeList";
+            case "자유게시판":
+                return "redirect:/board/forumList";
+            case "Q&A":
+                return "redirect:/board/qaList";
+            default:
+                return "redirect:/board/boardmain";
+        }
     }
 
     @GetMapping("/board/boarddelete/{boardNum}") //게시판 삭제
     public String noticeDelete(@PathVariable Long boardNum){
-        boardDao.delete(boardNum);
-        return "redirect:/board/boardmain";
+        String boardDivision = boardService.selectByNum(boardNum).getBoardDivision();
+        boardService.delete(boardNum);
+
+        switch (boardDivision) {
+            case "공지사항":
+                return "redirect:/board/noticeList";
+            case "자유게시판":
+                return "redirect:/board/forumList";
+            case "Q&A":
+                return "redirect:/board/qaList";
+            default:
+                return "redirect:/board/boardmain";
+        }
     }
 
-    @GetMapping("/board/noticeList") //공지사항 이동
-    public String notice(Model model, @RequestParam(value = "pageNo", required = false) String pageNoVal){
+    @GetMapping("/board/noticeList") //공지사항
+    public String notice(Model model, @RequestParam(value = "pageNo", required = false) String pageNoVal, HttpSession session){
         int pageNo = 1;
         if(pageNoVal != null){
             pageNo = Integer.parseInt(pageNoVal);
         }
 
-        List<Board> boardList = boardDao.select((pageNo - 1) * 10, 10, "공지사항");
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Emp emp = boardService.selectEmp(empId);
         BoardPage boardPage = listBoard.getBoardPage((long) pageNo, "공지사항");
 
-        model.addAttribute("boardList", boardList);
+        model.addAttribute("emp", emp);
         model.addAttribute("boardPage", boardPage);
         return "board/noticeList";
     }
 
-    @GetMapping("/board/forumList") //자유게시판 이동
+    @GetMapping("/board/forumList") //자유게시판
     public String forum(Model model, @RequestParam(value = "pageNo", required = false) String pageNoVal){
         int pageNo = 1;
         if(pageNoVal != null){
             pageNo = Integer.parseInt(pageNoVal);
         }
-
-        List<Board> boardList = boardDao.select((pageNo - 1) * 10, 10, "자유게시판");
         BoardPage boardPage = listBoard.getBoardPage((long) pageNo, "자유게시판");
 
-        model.addAttribute("boardList", boardList);
         model.addAttribute("boardPage", boardPage);
         return "board/forumList";
     }
 
-    @GetMapping("/board/qaList") //Q&A 이동
+    @GetMapping("/board/qaList") //Q&A
     public String qa(Model model, @RequestParam(value = "pageNo", required = false) String pageNoVal){
         int pageNo = 1;
         if(pageNoVal != null){
             pageNo = Integer.parseInt(pageNoVal);
         }
-
-        List<Board> boardList = boardDao.select((pageNo - 1) * 10, 10, "Q&A");
         BoardPage boardPage = listBoard.getBoardPage((long) pageNo, "Q&A");
 
-        model.addAttribute("boardList", boardList);
         model.addAttribute("boardPage", boardPage);
         return "board/qaList";
     }
@@ -122,10 +173,8 @@ public class BoardController {
             pageNo = Integer.parseInt(pageNoVal);
         }
 
-        List<Board> boardList = boardDao.select((pageNo - 1) * 10, 10, boardDivision, find);
         BoardPage boardPage = listBoard.getBoardPage((long) pageNo, boardDivision, find);
 
-        model.addAttribute("boardList", boardList);
         model.addAttribute("boardPage", boardPage);
         model.addAttribute("boardDivision", boardDivision);
         model.addAttribute("find", find);
@@ -140,10 +189,8 @@ public class BoardController {
             pageNo = Integer.parseInt(pageNoVal);
         }
 
-        List<Board> boardList = boardDao.select((pageNo - 1) * 10, 10, boardDivision, find);
         BoardPage boardPage = listBoard.getBoardPage((long) pageNo, boardDivision, find);
 
-        model.addAttribute("boardList", boardList);
         model.addAttribute("boardPage", boardPage);
         model.addAttribute("boardDivision", boardDivision);
         model.addAttribute("find", find);
@@ -151,17 +198,23 @@ public class BoardController {
     }
 
     @GetMapping("/board/replywrite/{boardNum}") //Q&A답변 폼
-    public String reply(@PathVariable Long boardNum, Model model){
-        Board board = boardDao.selectByNum(boardNum);
+    public String reply(@PathVariable Long boardNum, Model model, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Emp emp = boardService.selectEmp(empId);
+        Board board = boardService.selectByNum(boardNum);
 
+        model.addAttribute("emp", emp);
         model.addAttribute("board", board);
         return "board/replyWrite";
     }
 
     @PostMapping("/board/replywrite/{boardNum}") //Q&A답변 저장
-    public String replywrite(@PathVariable Long boardNum, @ModelAttribute("board") Board board){
-        Board pBoard = boardDao.selectByNum(boardNum);
-        boardDao.replywrite(pBoard, board);
+    public String replywrite(@PathVariable Long boardNum, @ModelAttribute("board") Board board, HttpSession session){
+        AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+        int empId = authInfo.getEmp_id();
+        Board pBoard = boardService.selectByNum(boardNum);
+        boardService.replywrite(pBoard, board, empId);
         return "redirect:/board/qaList";
     }
 }
